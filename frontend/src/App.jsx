bc909507c910b4b8346ce7abf7437d57c65514c1
anhtudo97 <anhtudo97@gmail.com>
2022-04-19 11:49:58 +0700

    feat(update): fe setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
